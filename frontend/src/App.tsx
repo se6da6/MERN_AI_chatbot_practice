0f6de6a037b50b8add816167dc90e1482727edba
se6da6 <sedaddak06@gmail.com>
2023-11-02 11:30:00 -0400

    authprovider created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,10 @@ import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import Signup from "./pages/Signup";
 import NotFound from "./pages/NotFound";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  console.log(useAuth()?.isLoggedIn);
   return (<main>
     <Header/>
     <Routes>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext.tsx';
 
 const theme= createTheme({typography:{
   fontFamily: "Roboto Slab, serif,",
@@ -13,10 +14,12 @@ const theme= createTheme({typography:{
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <AuthProvider>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
     </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
 )
