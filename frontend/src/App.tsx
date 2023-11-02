eacc35c7422bf876748a38c095396f5d73ebb22f
se6da6 <sedaddak06@gmail.com>
2023-11-02 09:46:15 -0400

    adding routes to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
 import Header from "./components/Header";
-
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+import Signup from "./pages/Signup";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (<main>
     <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/chat" element={<Chat/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
   </main>
   );
 }
