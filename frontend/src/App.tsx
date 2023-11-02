630d44054b9ee18d7a56b2d7ec84032150a79d37
se6da6 <sedaddak06@gmail.com>
2023-11-02 09:39:16 -0400

    adding pages and components to src

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
 
 
 function App() {
-  return <main>Hello</main>
+  return (<main>
+    <Header/>
+  </main>
+  );
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { createTheme, ThemeProvider } from '@mui/material'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
 
 const theme= createTheme({typography:{
   fontFamily: "Roboto Slab, serif,",
@@ -12,8 +13,10 @@ const theme= createTheme({typography:{
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
